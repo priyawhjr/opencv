5c27af8cc7398778505a04cdf297b1d85c946cef
priyawhjr <132220332+priyawhjr@users.noreply.github.com>
2026-08-18 22:13:02 +0530

    invisible_man
    Fixed syntax errors and improved code structure.

--- invis_man.py
+++ invis_man.py
@@ -1,3 +1,22 @@
+#Resourse link: https://www.geeksforgeeks.org/python/invisible-cloak-using-opencv-python-project/
+# You tube link : Youtube Link - https://www.youtube.com/watch?v=9P-yUFdXG-I
+#You can check source code on the project github repository, 
+
+#for input video and more details -  https://github.com/AdityaAtri/invisible_man
+
+
+"""we will learn how to create our own ‘Invisibility Cloak’ using 
+simple computer vision techniques in OpenCV. Here I have written this
+code in Python because python provides exhaustive and sufficient 
+library to build this program. Here, we will create this magical
+experience using an image processing technique called color detection 
+and segmentation. In order to run this code , you need a mp4 video 
+named "video.mp4". You must have a cloth of same color and no other
+color shoulf be there. I am taking red cloth. If you are taking some 
+other cloth , the code will remain the same but with minute changes"""
+
+
+
 import cv2 
 import numpy as np
 import time
